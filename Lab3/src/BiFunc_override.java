0c46ea4b38d1ec0bff242fda09c3c83710d57a52
Nynoko <46999342+Nynoko228@users.noreply.github.com>
2024-10-25 11:29:16 +0300

    Add files via upload

--- Lab3/src/BiFunc_override.java
+++ Lab3/src/BiFunc_override.java
@@ -1,17 +1,17 @@
+import java.math.BigDecimal;
 import java.util.function.BiFunction;
 public class BiFunc_override implements BiFunction<Integer, Integer, Integer>{
-
     @Override
     public Integer apply(Integer a, Integer b){
-        return (a+b);
+        return a+b;
     }
 
     public static void main(String[] args) {
         // Создаем экземпляр нашего класса
         BiFunction<Integer, Integer, Integer> arg = new BiFunc_override();
-
+        BigDecimal a = new BigDecimal(565676);
         // Пример использования
-        Integer result = arg.apply(10, 20);
+        Integer result = arg.apply(a.intValue(), 20);
         System.out.println("Результат сложения: " + result);
     }
 }
