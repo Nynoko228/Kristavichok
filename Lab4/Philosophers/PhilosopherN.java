a1114980cfb7a67bf92c70c67b205aec03041a19
Nynoko <krotov2020@gmail.com>
2024-11-21 22:19:37 +0300

    Дополнил философов

--- Lab4/Philosophers/PhilosopherN.java
+++ Lab4/Philosophers/PhilosopherN.java
@@ -1,4 +1,4 @@
-package org.example;
+package Philosophers;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -11,19 +11,19 @@ class Philosopher implements Runnable {
     private static final String YELLOW_COLOR = "\u001B[33m";
     private static final String GREEN_COLOR = "\u001B[32m";
 
-    private int philosopherNumber;
-    private Lock leftUtensil;
-    private Lock rightUtensil;
-    private Random randomGenerator;
-    private int minThoughtTime;
-    private int maxThoughtTime;
-    private int maxMealTime;
+    private final int philosopherNumber;
+    private final Lock leftFork;
+    private final Lock rightFork;
+    private final Random randomGenerator;
+    private final int minThoughtTime;
+    private final int maxThoughtTime;
+    private final int maxMealTime;
     private int mealsLeft;
 
-    public Philosopher(int number, Lock leftUtensil, Lock rightUtensil, int minThought, int maxThought, int maxMeal, int totalMeals) {
+    public Philosopher(int number, Lock leftFork, Lock rightFork, int minThought, int maxThought, int maxMeal, int totalMeals) {
         this.philosopherNumber = number;
-        this.leftUtensil = leftUtensil;
-        this.rightUtensil = rightUtensil;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
         this.randomGenerator = new Random();
         this.minThoughtTime = minThought;
         this.maxThoughtTime = maxThought;
@@ -31,12 +31,12 @@ class Philosopher implements Runnable {
         this.mealsLeft = totalMeals;
     }
 
-    private void reflect() throws InterruptedException {
+    private void thinking() throws InterruptedException {
         System.out.printf(YELLOW_COLOR + "Философ " + philosopherNumber + " размышляет\n" + RESET_COLOR);
         TimeUnit.MILLISECONDS.sleep(randomGenerator.nextInt(maxThoughtTime - minThoughtTime + 1) + minThoughtTime);
     }
 
-    private void dine() throws InterruptedException {
+    private void eating() throws InterruptedException {
         System.out.printf(GREEN_COLOR + "Философ " + philosopherNumber + " ест\n" + RESET_COLOR);
         TimeUnit.MILLISECONDS.sleep(randomGenerator.nextInt(maxMealTime));
     }
@@ -46,24 +46,30 @@ class Philosopher implements Runnable {
         try {
             while (true) {
                 if (mealsLeft == 0) {
+                    // Если философ слишком долго ждал, то он встаёт и уходит
                     System.out.printf(RED_COLOR + "Философ " + philosopherNumber + " покидает стол\n" + RESET_COLOR);
                     Thread.currentThread().interrupt();
                     break;
                 } else {
-                    reflect();
+                    thinking(); // Философ думает
                     mealsLeft--;
-                    if (leftUtensil.tryLock()) {
+                    // Философ подумал и решил попробовать взять левую вилку
+                    if (leftFork.tryLock()) {
                         try {
-                            if (rightUtensil.tryLock()) {
+                            // Философ решил попробовать взять ещё и правую вилку
+                            if (rightFork.tryLock()) {
                                 try {
-                                    dine();
+                                    // Философ поел и готов снова думать
+                                    eating();
                                     mealsLeft = 2;
                                 } finally {
-                                    rightUtensil.unlock();
+                                    // Философ кладёт правую вилку
+                                    rightFork.unlock();
                                 }
                             }
                         } finally {
-                            leftUtensil.unlock();
+                            // Философ кладёт левую вилку
+                            leftFork.unlock();
                         }
                     }
                 }
@@ -78,18 +84,13 @@ class DiningTable {
     private static final int NUMBER_OF_PHILOSOPHERS = 5;
 
     public static void main(String[] args) {
-        Lock[] utensils = new Lock[NUMBER_OF_PHILOSOPHERS];
+        Lock[] forks = new Lock[NUMBER_OF_PHILOSOPHERS];
         for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
-            utensils[i] = new ReentrantLock();
+            forks[i] = new ReentrantLock();
         }
 
-        Thread[] philosophers = new Thread[NUMBER_OF_PHILOSOPHERS];
         for (int i = 0; i < NUMBER_OF_PHILOSOPHERS; i++) {
-            philosophers[i] = new Thread(new Philosopher(i, utensils[i], utensils[(i + 1) % NUMBER_OF_PHILOSOPHERS], 1000, 3000, 500, 2));
-        }
-
-        for (Thread philosopher : philosophers) {
-            philosopher.start();
+            new Thread(new Philosopher(i, forks[i], forks[(i + 1) % NUMBER_OF_PHILOSOPHERS], 1000, 3000, 500, 2)).start();
         }
     }
 }
